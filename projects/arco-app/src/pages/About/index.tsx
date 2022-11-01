b3f9032cf577266365b11f9977ffe81937c5762d
gongxun <ex_xun.gong@avatr.com>
2022-11-01 17:24:32 +0800

    fix: 修改文件问题

--- projects/arco-app/src/pages/About/index.tsx
+++ projects/arco-app/src/pages/About/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Typography } from '@arco-design/web-react'
 
 const { Title } = Typography
 
 const About: React.FC = () => {
+  const [status, setStatus] = useState<boolean>(false)
+
+  useEffect(() => {
+    setStatus(true)
+    setTimeout(() => {
+      setStatus(false)
+    }, 3000)
+  }, [])
+
   return (
-    <Title type="primary" underline>
-      about page
-    </Title>
+    <>
+      <Title type="primary" underline>
+        about page {status}
+      </Title>
+    </>
   )
 }
 
