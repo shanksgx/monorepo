db957754938fe593b25c5c68b77308ab1a0b7808
goduer <526140067@qq.com>
2022-11-06 12:43:08 +0800

    feat: 自建next

---
+++ projects/arco-next/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default () => (
+  <>
+    <Head>
+      <title>My page title</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <div>Welcome to next.js!</div>
+  </>
+)
