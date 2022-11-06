c80dbcd602bcab6f122c1ab7dc1be5c3bfbc7400
goduer <526140067@qq.com>
2022-11-06 13:01:12 +0800

    feat: 添加head头属性

--- projects/arco-next/src/pages/index.tsx
+++ projects/arco-next/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Header } from 'components'
 
 export default () => (
   <>
-    <Header title={'hello goduer'} />
+    <Header title={'hello goduer'} description={'goduer is a coding!'} />
     <div>Welcome to next.js!</div>
   </>
 )
