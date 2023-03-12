0b7818a81afc7a84af4f49ed8659fe03d5cb352e
goduer <526140067@qq.com>
2023-03-12 18:43:26 +0800

    fix: edit page

---
+++ projects/taro-v3-template/config/index.js
@@ -0,0 +1,124 @@
+import ComponentsPlugin from 'unplugin-vue-components/webpack'
+import NutUIResolver from '@nutui/nutui-taro/dist/resolver'
+const path = require('path')
+
+const config = {
+  projectName: 'taro-v3-template',
+  date: '2023-3-7',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  // 开启 HTML 插件
+  plugins: ['@tarojs/plugin-html'],
+  defineConstants: {},
+  copy: {
+    patterns: [],
+    options: {}
+  },
+  framework: 'vue3',
+  compiler: 'webpack5',
+  cache: {
+    enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+  },
+  alias: {
+    '@': path.resolve(__dirname, '..', 'src')
+  },
+  // 配置全局 Scss 变量
+  sass: {
+    resource: [path.resolve(__dirname, '..', 'src/styles/index.scss')],
+    data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`
+  },
+  designWidth(input) {
+    // 配置 NutUI 375 尺寸
+    if (input?.file?.replace(/\\+/g, '/').indexOf('@nutui') > -1) {
+      return 375
+    }
+    // 全局使用 Taro 默认的 750 尺寸
+    return 750
+  },
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
+    375: 2 / 1
+  },
+  mini: {
+    webpackChain(chain) {
+      chain.merge({
+        module: {
+          rule: {
+            mjsScript: {
+              test: /\.mjs$/,
+              include: [/pinia/],
+              use: {
+                babelLoader: {
+                  loader: require.resolve('babel-loader')
+                }
+              }
+            }
+          }
+        }
+      })
+      chain.plugin('unplugin-vue-components').use(
+        ComponentsPlugin({
+          resolvers: [NutUIResolver({ taro: true })]
+        })
+      )
+    },
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {}
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024 // 设定转换尺寸上限
+        }
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  },
+  h5: {
+    publicPath: '/',
+    staticDirectory: 'static',
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {}
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    },
+    webpackChain(chain) {
+      chain.plugin('unplugin-vue-components').use(
+        ComponentsPlugin({
+          resolvers: [NutUIResolver({ taro: true })]
+        })
+      )
+    }
+  }
+}
+
+module.exports = function (merge) {
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'))
+  }
+  return merge({}, config, require('./prod'))
+}
