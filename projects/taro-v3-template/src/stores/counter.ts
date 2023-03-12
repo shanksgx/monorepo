e0e94591b433e110efdf474f454cee33a4620488
goduer <526140067@qq.com>
2023-03-12 18:45:17 +0800

    fix: edit test

--- projects/taro-v3-template/src/stores/counter.ts
+++ projects/taro-v3-template/src/stores/counter.ts
@@ -3,15 +3,15 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', {
   state: () => {
-    return { count: 0 }
+    return { count: 1 }
   },
   // could also be defined as
   // state: () => ({ count: 0 })
   actions: {
     increment() {
       this.count++
-    },
-  },
+    }
+  }
 })
 
 // You can even use a function (similar to a component setup()) to define a Store for more advanced use cases:
