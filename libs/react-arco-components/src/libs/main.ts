3a13a7211c3acfae897244a5512292f6c9fac40d
gongxun <gongxun@yccjk.com>
2023-07-13 14:38:23 +0800

    feat: 引入water-mark组件

--- libs/react-arco-components/src/libs/main.ts
+++ libs/react-arco-components/src/libs/main.ts
@@ -1,5 +1,6 @@
 export { default as ScrollBar } from "./components/ScrollBar";
 export { default as CustomButton } from "./components/CustomButton";
 export { default as SchamasForm } from "./components/SchamasForm";
+export { default as Watermark } from "./components/Watermark";
 
 export type { FormSchema } from "./components/SchamasForm";
